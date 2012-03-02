bf31cf54cbdd23564c6f0248c7c6739eb6b39a4f
Aaron Bedra <aaron@aaronbedra.com>
2012-03-02 10:32:35 -0600

    rename test file

---
+++ test/test_xpather.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'xpather'
+
+class TestXPather < MiniTest::Unit::TestCase
+  def setup
+    @document = XPather.new("test/books.xml")
+  end
+
+  def test_basic_search_one
+    assert_equal "Giada De Laurentiis", @document.search("/bookstore/book[1]/author") 
+  end
+
+  def test_basic_search_collection
+    assert_equal ["James McGovern", "Per Bothner", "Kurt Cagle", "James Linn", "Vaidyanathan Nagarajan"], 
+                 @document.search("/bookstore/book[3]/author")
+  end  
+end
