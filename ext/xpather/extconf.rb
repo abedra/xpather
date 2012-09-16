4562ea294ec55d46cfd70fc583ce6b33b9abbb17
Aaron Bedra <aaron@aaronbedra.com>
2012-09-16 10:57:52 -0400

    Add additional library paths to extconf.rb

--- ext/xpather/extconf.rb
+++ ext/xpather/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 have_library("xml2")
-find_header("libxml/tree.h", "/usr/include/libxml2")
-find_header("libxml/parser.h", "/usr/include/libxml2")
-find_header("libxml/xpath.h", "/usr/include/libxml2")
-find_header("libxml/xpathInternals.h", "/usr/include/libxml2")
+find_header("libxml/tree.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
+find_header("libxml/parser.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
+find_header("libxml/xpath.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
+find_header("libxml/xpathInternals.h", "/usr/include/libxml2", "/usr/local/include/libxml2")
 create_makefile('xpather/xpather')
